500de62b8a53469aa0f002293378d3f46074e7bf
Amine BOUDERHEM <mohamed-elamine.bouderhem@outlook.com>
2026-02-13 13:15:25 +0100

    Add configuration class (#10)
    * Add configuration class
    
    The clientConfig class will be used by the difference object of the sdk to get the configuration set by the user.
    The update of the context class will add the mutable context class as a whole part of the context to differentiate it from static part (that is not updatable)
    
    * remove togglesStorageTTL parameter

---
+++ include/unleash/Configuration/clientConfig.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <optional>
+#include <string>
+#include <utility>
+#include <chrono>
+#include <map>
+
+
+#include "unleash/Domain/context.hpp"
+#include "unleash/Domain/toggleSet.hpp"
+#include "unleash/Utils/utils.hpp"
+
+
+
+namespace unleash 
+{
+
+class Bootstrap final {
+public:
+    explicit Bootstrap(ToggleSet::Map p_map);
+    const ToggleSet::Map& getToggles() const;
+private: 
+    ToggleSet::Map _map;
+};
+
+class ClientConfig final {
+
+public:
+
+    
+    ClientConfig(const std::string& p_url, const std::string& p_clientKey, const std::string& p_appName);
+    //setters:
+    ClientConfig& setInstanceId(const std::string& p_instanceId);
+    ClientConfig& setRefreshInterval(utils::seconds s);
+    ClientConfig& setMetricsInterval(utils::seconds s);
+    ClientConfig& setMetricsIntervalInitial(utils::seconds s);
+    ClientConfig& setBootstrap(Bootstrap b);
+    ClientConfig& setBootstrapOverride(bool v);
+    ClientConfig& setHeaderName(std::string headerName);
+    ClientConfig& setCustomHeaders(std::map<std::string, std::string> headers);
+    ClientConfig& setImpressionDataAll(bool v);
+    ClientConfig& setUsePostRequests(bool v);
+    ClientConfig& setTimeOutQuery(utils::mSeconds m);
+    //ClientConfig& setTogglesStorageTTL(utils::seconds ttl);
+
+
+    //getters: 
+    const std::string& url() const;
+    const std::string& clientKey() const;
+    const std::string& appName() const;
+    const std::string& connectionId() const;
+    const std::string& instanceId() const;
+    utils::seconds refreshInterval() const;
+    utils::seconds metricsInterval() const;
+    utils::seconds metricsIntervalInitial() const;
+    const std::optional<Bootstrap>& bootstrap() const;
+    bool bootstrapOverride() const;
+    const std::string& headerName() const;
+    const std::map<std::string, std::string>& customHeaders() const;
+    bool impressionDataAll() const;
+    bool usePostRequests() const;
+    utils::mSeconds timeOutQuery() const;
+
+    bool isRefreshEnabled() const;
+    bool isMetricsEnabled() const;
+    
+
+    bool isValid();
+private: 
+
+    std::string _url;
+    std::string _clientKey;
+    std::string _appName;
+    std::string _connectionId;
+
+    utils::seconds _refreshInterval{0};
+    utils::seconds _metricsInterval{0};
+    utils::seconds _metricsIntervalInitial{0};
+    std::optional<Bootstrap> _bootstrap{};
+    bool _bootstrapOverride{true};
+    std::string _headerName =  std::string(utils::defaultHeadeName);
+    std::map<std::string, std::string> _customHeaders{};
+    bool _impressionDataAll{false};
+    bool _usePostRequests{false};
+    std::string _instanceId = std::string(utils::defaultInstanceId);
+    utils::mSeconds _timeOutQueryMS{5};
+
+    
+};
+
+
+}// namespace unleash
--- tests/test_context.cpp
+++ tests/test_context.cpp
@@ -3,11 +3,13 @@
 #include "unleash/Utils/utils.hpp"
 
 using unleash::Context;
+using unleash::MutableContext;
 
 TEST(ContextTest, ConstructorSetsRequiredFields)
 {
-    Context ctx("myApp", "123568941");
+    Context ctx("myApp", "environment",  "123568941");
     EXPECT_EQ(ctx.getAppName(), "myApp");
+    EXPECT_EQ(ctx.getEnvironment(), "environment");
     EXPECT_EQ(ctx.getSessionId(), "123568941");
 }
 
@@ -15,6 +17,7 @@ TEST(ContextTest, ConstructorSetNoField)
 {
     Context ctx{};
     EXPECT_EQ(ctx.getAppName(), std::string(utils::defaultAppName));
+    EXPECT_FALSE(ctx.getEnvironment().has_value());
     EXPECT_FALSE(ctx.getSessionId().empty());
     
 }
@@ -25,11 +28,6 @@ TEST(ContextTest, OptionalFieldsAreEmptyByDefault)
 {
     Context ctx("myApp");
     auto sessionId  = ctx.getSessionId();
-
-    EXPECT_FALSE(ctx.hasUserId());
-    EXPECT_FALSE(ctx.hasEnvironment());
-    EXPECT_FALSE(ctx.hasRemoteAddress());
-    EXPECT_FALSE(ctx.hasCurrentTime());
 
     EXPECT_FALSE(ctx.getUserId().has_value());
     EXPECT_FALSE(ctx.getEnvironment().has_value());
@@ -42,57 +40,78 @@ TEST(ContextTest, OptionalFieldsAreEmptyByDefault)
 
 TEST(ContextTest, SettersSetAndEmptyResets)
 {
-    Context ctx("myApp");
+    MutableContext mCtx{};
+
+    mCtx.setUserId("u1");
+    EXPECT_TRUE(mCtx.getUserId().has_value());
+    EXPECT_EQ(mCtx.getUserId().value(), "u1");
+
+    mCtx.setUserId(""); // should reset
+    EXPECT_FALSE(mCtx.getUserId().has_value());
+    mCtx.setRemoteAddress("10.0.0.1");
+    ASSERT_TRUE(mCtx.getRemoteAddress().has_value());
+    EXPECT_EQ(mCtx.getRemoteAddress().value(), "10.0.0.1");
+
+    mCtx.setRemoteAddress("");
+    EXPECT_FALSE(mCtx.getRemoteAddress().has_value());
+}
+
+TEST(ContextTest, SetMutableFieldsFromContextClass)
+{
+    Context ctx{};
 
     ctx.setUserId("u1");
-    ASSERT_TRUE(ctx.hasUserId());
-    EXPECT_EQ(*ctx.getUserId(), "u1");
+    EXPECT_TRUE(ctx.getUserId().has_value());
+    EXPECT_EQ(ctx.getUserId().value(), "u1");
 
     ctx.setUserId(""); // should reset
-    EXPECT_FALSE(ctx.hasUserId());
     EXPECT_FALSE(ctx.getUserId().has_value());
     ctx.setRemoteAddress("10.0.0.1");
-    ASSERT_TRUE(ctx.hasRemoteAddress());
-    EXPECT_EQ(*ctx.getRemoteAddress(), "10.0.0.1");
+    ASSERT_TRUE(ctx.getRemoteAddress().has_value());
+    EXPECT_EQ(ctx.getRemoteAddress().value(), "10.0.0.1");
 
     ctx.setRemoteAddress("");
-    EXPECT_FALSE(ctx.hasRemoteAddress());
+    EXPECT_FALSE(ctx.getRemoteAddress().has_value());
 }
+
+
 
 TEST(ContextTest, SetPropertyAddsAndUpdates)
 {
-    Context ctx("myApp");
+    MutableContext mCtx{};
 
-    ctx.setProperty("tenant", "acme");
-    ASSERT_EQ(ctx.getProperties().size(), 1u);
-    EXPECT_EQ(ctx.getProperties()[0].first, "tenant");
-    EXPECT_EQ(ctx.getProperties()[0].second, "acme");
+    mCtx.setProperty("tenant", "acme");
+    ASSERT_EQ(mCtx.getProperties().size(), 1u);
+    EXPECT_EQ(mCtx.getProperties()[0].first, "tenant");
+    EXPECT_EQ(mCtx.getProperties()[0].second, "acme");
 
     // Update existing key
-    ctx.setProperty("tenant", "acme2");
-    ASSERT_EQ(ctx.getProperties().size(), 1u);
-    EXPECT_EQ(ctx.getProperties()[0].second, "acme2");
+    mCtx.setProperty("tenant", "acme2");
+    ASSERT_EQ(mCtx.getProperties().size(), 1u);
+    EXPECT_EQ(mCtx.getProperties()[0].second, "acme2");
 }
 
 TEST(ContextTest, SetPropertyIgnoresEmptyAndReservedKeys)
 {
-    Context ctx("myApp");
+    MutableContext mCtx{};
 
-    ctx.setProperty("", "x");
-    EXPECT_TRUE(ctx.getProperties().empty());
+    mCtx.setProperty("", "x");
+    EXPECT_TRUE(mCtx.getProperties().empty());
 
     // reserved keys should be ignored by your current implementation
-    ctx.setProperty("userId", "x");
-    ctx.setProperty("appName", "x");
-    ctx.setProperty("currentTime", "x");
-    EXPECT_TRUE(ctx.getProperties().empty());
+    mCtx.setProperty("userId", "x");
+    mCtx.setProperty("appName", "x");
+    mCtx.setProperty("currentTime", "x");
+    EXPECT_TRUE(mCtx.getProperties().empty());
 }
 
 TEST(ContextTest, SetCurrentTimeProperty)
 {
     Context ctx("myApp");
-    EXPECT_FALSE(ctx.hasCurrentTime());
-
-    ctx.setCurrentTime();
-    EXPECT_TRUE(ctx.hasCurrentTime());
+    EXPECT_FALSE(ctx.getCurrentTime().has_value());
+    MutableContext mCtx{};
+    mCtx.setCurrentTime();
+    ctx.updateMutableContext(mCtx);
+    EXPECT_TRUE(ctx.getCurrentTime().has_value());
 }
+
